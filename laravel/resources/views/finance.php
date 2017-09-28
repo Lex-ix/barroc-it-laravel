cec0d3e97d41fe3cf8fa1b8655618baf1283c560
alex <d228788@edu.rocwb.nl>
2017-09-28 12:13:48 +0200

    Revert "Revert "finance.php added""
    This reverts commit 5f5545db36dc8d66ea69c887ff2f6d9ed68cedeb.

---
+++ laravel/resources/views/finance.php
@@ -0,0 +1,84 @@
+<?php
+    require_once('header.php');
+    require_once('nav.php');
+    ?>
+    
+    <div class="main-content">
+        <div class="container">
+                <h2 class="subhead">company's lagging behind.</h2>
+                <table class="finance">
+                    <tr>
+                        <td>company_name</td>
+                        <td>
+                            <form action="">
+                                <input type="submit" value="-">
+                            </form>
+                        </td>
+    
+                        <td>Hoeveelheid in het rood</td>
+                        
+                        <td>
+                            <form action="">
+                                <input type="submit" value="+">
+                            </form>
+                        </td>
+    
+                        <td>
+                            <form action="">
+                                <input type="submit" value="Invoeren factuur">
+                            </form>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>company_name</td>
+                        <td>
+                            <form action="">
+                                <input type="submit" value="-">
+                            </form>
+                        </td>
+                        
+                        <td>Hoeveelheid in het rood</td>
+                        
+                        <td>
+                            <form action="">
+                                <input type="submit" value="+">
+                            </form>
+                        </td>
+    
+                        <td>
+                            <form action="">
+                                <input type="submit" value="Invoeren factuur">
+                            </form>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>company_name</td>
+                        <td>
+                            <form action="">
+                                <input type="submit" value="-">
+                            </form>
+                        </td>
+    
+                        <td>Hoeveelheid in het rood</td>
+                        
+                        <td>
+                            <form action="">
+                                <input type="submit" value="+">
+                            </form>
+                        </td>
+    
+                        <td>
+                            <form action="">
+                                <input type="submit" value="Invoeren factuur">
+                            </form>
+                        </td>
+                    </tr>
+                </table>
+        </div>
+    </div>
+    
+<?php
+    require_once('footer.php');
+    ?>
