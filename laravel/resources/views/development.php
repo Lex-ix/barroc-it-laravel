316276f0895d647232e2bd0c007da9bf9e58c007
arminkf <d229606@edu.rocwb.nl>
2017-09-29 12:10:59 +0200

    development pages & subs

---
+++ laravel/resources/views/development.php
@@ -0,0 +1,114 @@
+<?php
+require_once "header.php";
+require_once "nav.php";
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+
+    <div class="projectstatus">
+        <div class="container">
+            <h1> Development Home Scherm</h1>
+            <div class="start">
+                <a href="devstart.php" target="_blank">Project Start</a>
+            </div>
+        </div>
+        <div class="finish">
+            <div class="container">
+                <a href="devfinish.php" target="_blank">Project Afronden</a>
+            </div>
+        </div>
+    </div>
+    <div class="customerstatus">
+        <div class="container">
+            <h2 class="subhead">Table</h2>
+            <table>
+                <tr>
+                    <th>Thing 1 Lorem Ipsum</th>
+                    <th>Thing 2 Lorem Ipsum</th>
+                    <th>Thing 3 Lorem Ipsum</th>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+            </table>
+        </div>
+        </section>
+    </div>
+
+    </div>
+    <div class="pausedstatus">
+        <div class="container">
+            <h2 class="subhead">Table</h2>
+            <table>
+                <tr>
+                    <th>Thing 1 Lorem Ipsum</th>
+                    <th>Thing 2 Lorem Ipsum</th>
+                    <th>Thing 3 Lorem Ipsum</th>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+            </table>
+        </div>
+        </section>
+    </div>
+
+    </div>
+
+    </body>
+    </html>
+
+<?php
+require_once "footer.php";
---
+++ laravel/resources/views/devfinish.php
@@ -0,0 +1,60 @@
+<?php
+require_once "header.php";
+require_once "nav.php";
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <div class="customerstatus">
+        <div class="container">
+            <h1>Gepauzeerde projecten</h1>
+            <h2 class="subhead">Table</h2>
+            <table>
+                <tr>
+                    <th>Thing 1 Lorem Ipsum</th>
+                    <th>Thing 2 Lorem Ipsum</th>
+                    <th>Thing 3 Lorem Ipsum</th>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+                <tr>
+                    <td>Thing 1</td>
+                    <td>Thing 2</td>
+                    <td>Thing 3</td>
+                </tr>
+            </table>
+        </div>
+        </section>
+    </div>
+
+    </div>
+    </body>
+    </html>
+<?php
+require_once "footer.php";
+
