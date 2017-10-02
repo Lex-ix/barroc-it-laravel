4389e0d2ed3caf4d7811442c6efe8e6d4fbe9302
alex <d228788@edu.rocwb.nl>
2017-10-02 12:11:47 +0200

    finance + css update

--- laravel/resources/views/finance.php
+++ laravel/resources/views/finance.php
@@ -77,64 +77,137 @@
                 </tr>
             </table>
     </div>
-    <div class="container">
-        <form action="">
-            <label for="customer">Customer ID</label>
-            <select name="customer" id="customer">
-                <option value="">Select</option>
-                <option value="">------------</option>
-                
-                <option value="">498465</option>
-                <option value="">61651615</option>
-                <option value="">61516</option>
-                <option value="">84561</option>
-                <option value="">16541</option>
-            </select>
-        </form>
+    <div class="background">
+        <div class="container">
+            <div class="request-info">
+                <h2 class="subhead">Request information</h2>
+            <form action="">
+                <label for="customer">Customer ID</label>
+                <select name="customer" id="customer">
+                    <option value="">Select</option>
+                    <option value="">------------</option>
+                    
+                    <option value="">498465</option>
+                    <option value="">61651615</option>
+                    <option value="">61516</option>
+                    <option value="">84561</option>
+                    <option value="">16541</option>
+                </select>
+                <input type="submit" value="Request information">
+            </form>
+            </div>
+        </div>
+    
+            <div class="tbl-finance">
+                <table>
+                    <tr>
+                        <th>Company ID</th>
+                        <th>Name</th>
+                        <th>Last name</th>
+                        <th>Initials</th>
+                        <th>Company name</th>
+                        <th>Unpaid invoices</th>
+                        <th>Bank account Nr.</th>
+                        <th>VAT Nr.</th>
+                        <th>Ledger Nr.</th>
+                        <th>E-mail</th>
+                        <th>Fax Nr.</th>
+                        <th>balance</th>
+                    </tr>
+                    <tr class="gerrie">
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                        <td>Thing 4</td>
+                        <td>Thing 5</td>
+                        <td>Thing 6</td>
+                        <td>Thing 7</td>
+                        <td>Thing 8</td>
+                        <td>Thing 9</td>
+                        <td>Thing 10</td>
+                        <td>Thing 11</td>
+                        <td>Thing 12</td>
+                    </tr>
+                    <tr class="gerrie">
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                        <td>Thing 4</td>
+                        <td>Thing 5</td>
+                        <td>Thing 6</td>
+                        <td>Thing 7</td>
+                        <td>Thing 8</td>
+                        <td>Thing 9</td>
+                        <td>Thing 10</td>
+                        <td>Thing 11</td>
+                        <td>Thing 12</td>
+                    </tr>
+                    <tr class="gerrie">
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                        <td>Thing 4</td>
+                        <td>Thing 5</td>
+                        <td>Thing 6</td>
+                        <td>Thing 7</td>
+                        <td>Thing 8</td>
+                        <td>Thing 9</td>
+                        <td>Thing 10</td>
+                        <td>Thing 11</td>
+                        <td>Thing 12</td>
+                    </tr>
+                    <tr class="gerrie">
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                        <td>Thing 4</td>
+                        <td>Thing 5</td>
+                        <td>Thing 6</td>
+                        <td>Thing 7</td>
+                        <td>Thing 8</td>
+                        <td>Thing 9</td>
+                        <td>Thing 10</td>
+                        <td>Thing 11</td>
+                        <td>Thing 12</td>
+                    </tr>
+                    <tr class="gerrie">
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                        <td>Thing 4</td>
+                        <td>Thing 5</td>
+                        <td>Thing 6</td>
+                        <td>Thing 7</td>
+                        <td>Thing 8</td>
+                        <td>Thing 9</td>
+                        <td>Thing 10</td>
+                        <td>Thing 11</td>
+                        <td>Thing 12</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
-    <div class="tbl-finance">
-            <table>
-                <tr>
-                    <th>Company ID</th>
-                    <th>Name</th>
-                    <th>Last name</th>
-                    <th>Initials</th>
-                    <th>Company name</th>
-                    <th>Unpaid invoices</th>
-                    <th>Bank account number</th>
-                    <th>VAT number</th>
-                    <th>Ledger number</th>
-                    <th>E-mail</th>
-                    <th>Fax number</th>
-                    <th>balance</th>
-                </tr>
-                <tr>
-                    <td>Thing 1</td>
-                    <td>Thing 2</td>
-                    <td>Thing 3</td>
-                </tr>
-                <tr>
-                    <td>Thing 1</td>
-                    <td>Thing 2</td>
-                    <td>Thing 3</td>
-                </tr>
-                <tr>
-                    <td>Thing 1</td>
-                    <td>Thing 2</td>
-                    <td>Thing 3</td>
-                </tr>
-                <tr>
-                    <td>Thing 1</td>
-                    <td>Thing 2</td>
-                    <td>Thing 3</td>
-                </tr>
-                <tr>
-                    <td>Thing 1</td>
-                    <td>Thing 2</td>
-                    <td>Thing 3</td>
-                </tr>
-            </table>
-    </div>
+    
+        <div class="container">
+            <div class="pause-project">
+                <h2 class="subhead">Pause Project</h2>
+            <form action="">
+                <label for="customer">Customer ID</label>
+                <select name="customer" id="customer">
+                    <option value="">Select</option>
+                    <option value="">------------</option>
+
+                    <option value="">498465</option>
+                    <option value="">61651615</option>
+                    <option value="">61516</option>
+                    <option value="">84561</option>
+                    <option value="">16541</option>
+                </select>
+                <input type="submit" value="Pause project">
+            </form>
+            </div>
+        </div>
 </div>
     
 <?php
