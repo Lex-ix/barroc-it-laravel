bdcad5698ea8f486ac8f1dbf9f12c58eda07e804
arminkf <d229606@edu.rocwb.nl>
2017-10-03 09:35:24 +0200

    dev/css

--- laravel/resources/views/development.php
+++ laravel/resources/views/development.php
@@ -2,109 +2,111 @@
 require_once "header.php";
 require_once "nav.php";
 ?>
-    <div class="development
-        <div class="projectstatus">
-            <div class="container">
-                <h1> Development Home Scherm</h1>
-                <div class="start">
-                    <a href="dev_start" target="_blank">Start project</a>
-                </div>
+
+<div class="projectstatus">
+    <div class="container">
+        <h2 class="subhead"> Development Home Scherm</h2>
+          <div class="projects">
+            <div class="start">
+                <a href="dev_start" target="_blank" class="button">Start project</a>
             </div>
             <div class="finish">
                 <div class="container">
-                    <a href="dev_finish" target="_blank">Finish project</a>
+                    <a href="dev_finish" target="_blank" class="button">Finish project</a>
                 </div>
             </div>
-        </div>
-        <div class="customerstatus">
-            <div class="container">
-                <h2 class="subhead">client status</h2>
-                <table>
-                    <tr>
-                        <th>project_id</th>
-                        <th>company_id</th>
-                        <th>application_Desc</th>
-                        <th>maintenance</th>
-                        <th>operating_system</th>
-                        <th>finished</th>
-                        <th> hardware_desc</th>
-
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                </table>
-            </div>
-            </section>
-        </div>
-
-        </div>
-        <div class="pausedstatus">
-            <div class="container">
-                <h2 class="subhead">paused projects</h2>
-                <table>
-                    <tr>
-                        <th>project_id</th>
-                        <th>company_id</th>
-                        <th>application_Desc</th>
-                        <th>maintenance</th>
-                        <th>operating_system</th>
-                        <th>finished</th>
-                        <th> hardware_desc</th>
-                        <th>paused</th>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                    <tr>
-                        <td>Thing 1</td>
-                        <td>Thing 2</td>
-                        <td>Thing 3</td>
-                    </tr>
-                </table>
-            </div>
-            </section>
-        </div>
+          </div>
     </div>
+
+<div class="customerstatus">
+    <div class="container">
+        <h2 class="subhead">client status</h2>
+        <table>
+            <tr>
+                <th>project_id</th>
+                <th>company_id</th>
+                <th>application_Desc</th>
+                <th>maintenance</th>
+                <th>operating_system</th>
+                <th>finished</th>
+                <th> hardware_desc</th>
+
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+        </table>
+    </div>
+    </section>
+</div>
+
+</div>
+<div class="pausedstatus">
+    <div class="container">
+        <h2 class="subhead">paused projects</h2>
+        <table>
+            <tr>
+                <th>project_id</th>
+                <th>company_id</th>
+                <th>application_Desc</th>
+                <th>maintenance</th>
+                <th>operating_system</th>
+                <th>finished</th>
+                <th> hardware_desc</th>
+                <th>paused</th>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+            <tr>
+                <td>Thing 1</td>
+                <td>Thing 2</td>
+                <td>Thing 3</td>
+            </tr>
+        </table>
+    </div>
+    </section>
+</div>
+
 
 <?php
 require_once "footer.php";
