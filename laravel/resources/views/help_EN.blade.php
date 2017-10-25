40c8f6f47323ea2a4fa22f839a39e4ecfe11850c
alex <d228788@edu.rocwb.nl>
2017-10-25 18:24:54 +0200

    help function update

--- laravel/resources/views/help_EN.blade.php
+++ laravel/resources/views/help_EN.blade.php
@@ -1,14 +1,79 @@
 @extends('master')
 @section('content')
 
-<section>
-    <div class="container">
-        <h2 class="subhead">Login</h2>
-        <p>
-            To be able to log in you must enter your username and password. If you do not know your username or password, you can ask your manager.
-            Once you've entered your credentials you must click on the submit button to log in.
-        </p>
-    </div>
-</section>
+    <section>
+        <div class="container">
+            <h2 class="subhead">Login</h2>
+            <p>
+                If you want to login you will have to know your username and password first, if you don't know these then you should ask this to you your manager or to a co-worker
+            </p>
+            <p>
+                If u have all your information then you can fill it in, when you have filled it in you can click 'submit' to login.
+            </p>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2 class="subhead">Finance</h2>
+            <p>
+                On the finance home-page you can see all companies that are active.
+            </p>
+            <p>
+                If you click on the 'request info' button then you can see all the specific information of the selected company.
+            </p>
+            <p>
+                After that, if you click on the 'Insert invoice' button you will get a form where you can add an invoice.
+            </p>
+            <p>
+                If you then click on 'insert invoice' the balance of the customer will be updated automatically.
+            </p>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2 class="subhead">Sales</h2>
+            <p>
+                On the sales home-page you can see all companies that have debt en all the companies that don't have debt.
+            </p>
+            <p>
+                If you click on the 'add cutomer' button then you can add an customer, fill in all the required information and click on the 'add' button.
+            </p>
+            <p>
+                If you click on the 'delete' button, you won't delete the customer permanently, they will be set to inactive.
+            </p>
+            <p>
+                If you click on the 'details' button, you will go to the details page of the selected customer, here you will see all the specific details of the customer.
+            </p>
+            <p>
+                On the details page you cna click on the 'create project' button, there you will need to fill in a form with all the required information and click on 'create', then the project will be added to the customers detail page.
+            </p>
+            <p>
+                At the project on the detail page of the customer is a 'delete' button, if you click on this you won't delete the project permanently but set it to inactive.
+            </p>
+            <p>
+                At the project on the detail page of the customer is also a 'details' button, if you click on this you will go to the details page of the selected project.
+            </p>
+            <p>
+                On the detail page of the project you can see all specific details about the project.
+            </p>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2 class="subhead">Development</h2>
+            <p>
+                On the development home-page you can see the statuses of the customers and which projects are paused.
+            </p>
+            <p>
+                If you click on the 'projects' button you can see all the queued projects and all active projects.
+            </p>
+            <p>
+                If you then click on the 'view project' button you can see all the specific details of the selected project and make it active, if the project is already active you will have the posibility to finish it.
+            </p>
+        </div>
+    </section>
 
 @endsection
--- laravel/resources/views/help_NL.blade.php
+++ laravel/resources/views/help_NL.blade.php
@@ -6,7 +6,72 @@
         <h2 class="subhead">Login</h2>
         <p>
             Als u in wil loggen dan moet je eerst je gebruikersnaam en wachtwoord weten, als u deze niet weet dan zou u dit aan uw manager kunnen vragen of aan een collegda.
+        </p>
+        <p>
             Als u uw gegevens heeft dan kan u ze invullen, als u dit gedaan heeft dan klikt u up 'submit' om in te loggen.
+        </p>
+    </div>
+</section>
+
+    <section>
+        <div class="container">
+            <h2 class="subhead">Finance</h2>
+            <p>
+                Op de finance start pagina ziet u alle bedrijven die actief zijn.
+            </p>
+            <p>
+                Als u op de 'request info' knop drukt bij een bedrijf ziet u de specifieke gegeven dan het geselecteerde bedrijf.
+            </p>
+            <p>
+                Als u daarna op de 'Insert invoice' knop drukt komt u bij een form waar u een factuur kunt toevoegen.
+            </p>
+            <p>
+                Als u dan op 'Insert invoice' drukt word de balance automatisch geupdate.
+            </p>
+        </div>
+    </section>
+
+<section>
+    <div class="container">
+        <h2 class="subhead">Sales</h2>
+        <p>
+            Op de Sales startpagina kunt u alle klanten zien die schulden hebben en alle klanten die geen schulden hebben.
+        </p>
+        <p>
+            Als u op 'add customer' drukt kunt u een klant toevoegen, vul al zijn informatie in en druk dan op 'add'.
+        </p>
+        <p>
+            Als u op 'delete' drukt verwijderd u een klant maar niet permanent, hij word dan op inactief gezet.
+        </p>
+        <p>
+            Als u op 'Details' drukt gaat u naar de detail pagina van het bedrijf dat u wilde zien, hier staan alle specifieke details van de klant.
+        </p>
+        <p>
+            Op de detail pagina kunt u op 'create project' klikken, daar kunt u een form invullen met alle info en op 'create' drukken, dan komt het project op de detail pagina van het bedrijf te staan.
+        </p>
+        <p>
+            Bij het project op de detail pagina van het bedrijf staat een 'delete' knop waarmee u het project niet permanent delete maar inactief zet.
+        </p>
+        <p>
+            Bij het project op de detail pagina van het bedrijf staat ook een 'details' knop waarmee u naar de detail pagina van het gekozen project gaat.
+        </p>
+        <p>
+            Op de details pagina van het project kunt u alle details van het project zien.
+        </p>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <h2 class="subhead">Development</h2>
+        <p>
+            Op de Development startpagina kunt u de statussen van de klanten zien en welke projecten op pauze staan.
+        </p>
+        <p>
+            Als u op 'Projects' drukt kunt u alle projecten zien die in de wachtrij staan en alle actieve projecten.
+        </p>
+        <p>
+            Als u op 'view project' drukt kunt u alle details van het gekozen project zien en deze actief zetten, als het project al actief is heeft u de mogelijkheid om hem te finishen.
         </p>
     </div>
 </section>
